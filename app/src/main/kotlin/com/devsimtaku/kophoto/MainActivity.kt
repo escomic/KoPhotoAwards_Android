0267a5a254a99a3fff50cf4493dc746e2520450f
essim <escomicv@gmail.com>
2026-05-10 17:15:59 +0900

    feat: 엣지 투 엣지 UI의 시스템 바 스타일 명시

--- app/src/main/kotlin/com/devsimtaku/kophoto/MainActivity.kt
+++ app/src/main/kotlin/com/devsimtaku/kophoto/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.devsimtaku.kophoto
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.devsimtaku.kophoto.core.designsystem.theme.KoPhotoTheme
@@ -12,7 +14,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT,
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                scrim = Color.TRANSPARENT,
+                darkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b),
+            ),
+        )
         setContent {
             KoPhotoTheme {
                 KoPhotoApp()
